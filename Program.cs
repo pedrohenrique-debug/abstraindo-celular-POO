3cd4fd4e0adba9e53b908d36773e5b760772c3b9
rique <pedrohenriqueboy000@gmail.com>
2022-09-29 09:44:32 -0300

    primeira versao

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
